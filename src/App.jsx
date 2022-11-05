71bdd86e830c999f1474b6d7da4f7342357e08f5
Rodrigo <rodrydanielmir@gmail.com>
2022-11-05 01:35:41 -0300

    se agregaron elementos

---
+++ src/App.jsx
@@ -0,0 +1,101 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import RickItem from './components/ResidentInfo';
+import frontPage from '../src/assets/image 3.png';
+import inFrontPage from '../src/assets/image 2.png';
+import RickLoad from "./assets/image_processing20210907-11935-193el2x.gif";
+
+import Pagiantion  from "./components/Pagiantion";
+
+
+function App() {
+
+  const[location, setLocation]= useState({})
+  const[inputId, setInputId]= useState("Escribe el nombre de la ubicación")
+  const [loading, setLoading]=useState(false);
+ const [pageNumber,updatePageNumber]=useState(1);
+let info=0;
+  useEffect(() => {
+    const randomId = Math.floor(Math.random ()*126)
+    axios.get(`https://rickandmortyapi.com/api/location/${randomId}`)
+      .then(res => setLocation(res.data))
+      /*---------Loading---------*/ 
+      setLoading(true)
+      info=location.character?.pages
+setTimeout(()=>{
+  setLoading(false)
+},5000);
+},[])
+
+  console.log(location);
+  
+
+  const searchId = () => {
+    
+    axios.get(`https://rickandmortyapi.com/api/location/${inputId}`)
+      .then(res => setLocation(res.data))
+      info=location.character?.pages
+  }
+
+
+
+  return (
+    
+    <>
+    {
+           loading ?
+           <div className='Loader'>
+            <img src={RickLoad} alt="" />
+           <h1 className='Loading'>Loading...</h1>
+             </div>
+           :
+
+    <div className="App" style={{background: "#242635"}}>
+      
+      <div className='banner'>
+        <img className='frontPage' src={frontPage} alt="" />
+        <div className='divFrontPage'>
+          <img className='inFrontPage' src={inFrontPage} alt="" />
+        </div>
+      </div>
+      
+      <div className='divSearch'>
+        <div className='search'>
+          <input className='input' type="text" value={inputId} onChange={e => setInputId(e.target.value)}/>
+          <button className=' seach-button' onClick={searchId}>Search</button>
+        </div>
+
+        <div className='divInfo'>
+          <div className='infoUno'><p>Nombre: <br /> <br /> {location.name} </p></div>
+          <div className='infoDos'><p>Dimensión: <br /> <br /> {location.dimension}</p></div>
+          <div className='infoTres'><p>Tipo: <br /> <br /> {location.type}</p></div>
+          <div className='infoCuatro'><p>Población: <br />  <br />{location.residents?.length} </p></div>
+        </div>
+        
+      
+
+         <div >
+            <ul className='charactersContainer'>
+              {location.residents?.map((residents) => (
+              
+                <RickItem key={residents} residents={residents}/>
+              ))}
+            </ul>
+            <Pagiantion
+        location={location}
+        pageNumber={pageNumber}
+        updatePageNumber={updatePageNumber}
+      />
+        </div>
+      </div>
+     
+     
+
+    </div>
+     }   
+     </>
+  )
+}
+
+export default App
---
+++ src/components/ResidentInfo.jsx
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
+
+const ResidentInfo = ({residents}) => {
+
+    const [characters, setCharacter] = useState({})
+
+
+    useEffect(() => {
+        axios.get(residents)
+            .then(res => setCharacter(res.data))
+    }, [])
+
+    console.log(characters);
+
+    return (
+        <>
+        <div className='divCharacters'>
+            <li>
+                <img className='image' src={characters.image} alt="" />
+                <div className='infoCharacters'>
+                    <h3>{characters.name}</h3>
+                    <p>Status: {characters.status}{" "}-{" "}{characters.type} </p>
+                    <p>Origin: <br /> {characters.origin?.name} </p>
+                    <p>episodes where appear: <br />{characters.episode?.length}</p>
+                </div>
+            </li>
+           
+        </div>
+       
+        </>
+    );
+};
+
+export default ResidentInfo;
